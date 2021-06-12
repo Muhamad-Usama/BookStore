e5823659aa8a8e825a128c9e027417f2e3742229
Muhammad_Usama <03068021348usa@gmail.com>
2021-06-12 23:15:41 +0500

    Done Controller

--- BookA/BookA/Startup.cs
+++ BookA/BookA/Startup.cs
@@ -16,6 +16,7 @@ namespace BookA
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,16 +26,29 @@ namespace BookA
             {
                 app.UseDeveloperExceptionPage();
             }
+            //app.Use(async(context, next)=>
+            //    {
+            //        await context.Response.WriteAsync("Hello from 1st middleware");
+            //        await next();
+            //        await context.Response.WriteAsync("Hello from 2nd middleware");
+            //        await next();
+            //        await context.Response.WriteAsync("Hello from 3rd middleware");
+            //    });
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
+            app.UseEndpoints(endpoint =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoint.MapDefaultControllerRoute();
             });
+            
         }
     }
 }
